07ec8b3c9f044933f5a7fe1cbcfcdae4f990c262
EmmaLitzer <42912879+EmmaLitzer@users.noreply.github.com>
2022-04-14 10:03:39 -0500

    update for new addr

--- Lab8/Lab8_python_v2.py
+++ Lab8/Lab8_python_v2.py
@@ -17,16 +17,30 @@ bit_file = '' + '.bit'										# bit file name in same folder as python file
 dev = ok.okCFrontPanel()  # define a device for FrontPanel communication
 SerialStatus=dev.OpenBySerial("")      # open USB communicaiton with the OK board
 
-def Write_Grab_FSM(PC_data, rw):
+
+zeros8 = '00000000'
+read_bit = '00000000'
+write_bit = '00000001'
+
+registers = {'raddr3':'0000011',
+	     'radar4':'0000100',
+	     
+	    }
+read_write = {'raddr3_w':"0" + registers['raddr3'] + '00101001' + zeros8 + write_bit,
+	      'raddr3_r':"0" + registers['raddr3'] + '00101001' + zeros8 + write_bit,
+	     'raddr4_w': "0" + registers['raddr4'] + '10001001' + zeross8 +write_bit,
+	     'raddr4_r': "0" + registers['raddr4'] + '10001001' + zeross8 + write_bit,
+	     }
+
+
+
+
+def Write_Grab_FSM(rw):
 	PC_control = 1																				# turn FSM on
-	if rw=='write':
-		# write sequence
-		PC_data = "00000011001010010000000000000001"
-	elif rw=='read':
-		# read sequence
-		PC_data = "00000011001010010000000000000000"
+	if rw in read_write.keys():
+		PC_Data=read_write[rw]
 	else:
-		print("Please put 'write' or 'read' into Write_Grab_FSM")
+		print("Please put a correct key into Write_Grab_FSM")
 		
 	# send read/write to PC and grab data into dev
 	dev.SetWireInValue(0x00, PC_control)                # send in value of 1 to start the FSM
@@ -43,10 +57,19 @@ def Write_Grab_FSM(PC_data, rw):
 # MAY NEED TO CHANGE WIRE NUMBERS (0x##)
 try:                                                        # run temperature loop until ^C is pressed in terminal
         # write sequence
-				Write_Grab_FSM('write')
-        
-        #read sequence
-				Write_Grab_FSM('read')
+	Write_Grab_FSM('raddr3_w')
+	Write_Grab_FSM('raddr4_w')
+	Write_Grab_FSM('raddr3_r')
+	
+	dev.UpdateWireOuts()                                # FSM sends Temp data to PC
+        output = dev.GetWireOutValue(0x20)                  # get msb temp register and shift 8 bits to the left
+        print(output)
+	
+	Write_Grab_FSM('raddr4_r')
+	dev.UpdateWireOuts()                                # FSM sends Temp data to PC
+        output = dev.GetWireOutValue(0x20)                  # get msb temp register and shift 8 bits to the left
+        print(output)
+	
     
         dev.UpdateWireOuts()                                # FSM sends Temp data to PC
         output = dev.GetWireOutValue(0x20)                  # get msb temp register and shift 8 bits to the left
