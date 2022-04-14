a406b36149a62184cdf1b8a6d47810fb5f9352b8
Megan Chung <54920114+meganchung@users.noreply.github.com>
2022-04-14 10:06:17 -0500

    Update motorcontroltest.py

--- Lab7/motorcontroltest.py
+++ Lab7/motorcontroltest.py
@@ -16,18 +16,16 @@ SerialStatus=dev.OpenBySerial("")      # open USB communicaiton with the OK boar
 # Define the two variables that will send data to the FPGA
 # We will use WireIn instructions to send data to the FPGA
 motor_control = 1
-Pulses = 2
-dirinput = 0
-dutycyle = 60
+Pulses = 100
+dirinput = 0 #0 is backwards, 1 is forwards
+dutycyle = 100
 
 dev.SetWireInValue(0x00, motor_control) #Input data for Variable 1 using mamoery space 0x00
 dev.SetWireInValue(0x01, Pulses) #Input data for Variable 2 using mamoery space 0x01
 dev.SetWireInValue(0x02, dirinput) #Input data for Variable 2 using mamoery space 0x02
 dev.SetWireInValue(0x03, dutycyle) #Input data for Variable 2 using mamoery space 0x03
 dev.UpdateWireIns()  # Update the WireIns
-
-
-time.sleep(0.0001)                 
+               
 motor_control = 0
 dev.SetWireInValue(0x00, motor_control)
 dev.UpdateWireIns()
