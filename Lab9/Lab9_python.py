12850309673d4078732e035b117fd7659e8d85f2
Megan Chung <54920114+meganchung@users.noreply.github.com>
2022-04-18 18:30:06 -0500

    Update Lab9_python.py

--- Lab9/Lab9_python.py
+++ Lab9/Lab9_python.py
@@ -3,6 +3,8 @@ import sys    # system related library
 ok_loc = 'C:\Program Files\Opal Kelly\FrontPanelUSB\API\Python\3.6\x64'
 sys.path.append(ok_loc)   # add the path of the OK library
 import ok     # OpalKelly library
+import numpy as np
+import matplotlib.pyplot as plt
 
 #%% 
 # Define FrontPanel device variable, open USB communication and
@@ -32,19 +34,42 @@ print("----------------------------------------------------")
 print("----------------------------------------------------")
 
 #%% 
+# Need to set SPI settings and start with FSM matching CMV300 data sheet
+
+
+
 dev.SetWireInValue(0x00, 1); #Reset FIFOs and counter
 dev.UpdateWireIns();  # Update the WireIns
 
 dev.SetWireInValue(0x00, 0); #Release reset signal
 dev.UpdateWireIns();  # Update the WireIns
  
-buf = bytearray(1024*4);
-dev.ReadFromBlockPipeOut(0xa0, 1024, buf);  # Read data from BT PipeOut
+
+# Data transfer length multiple of 16
+# length of data transfer = image multiple of block size
+# Total num of data points = 648 x 488 x 1Byte/pix = 316224 Bytes
+
+
+transfer_length = 4 #16
+pix1 = 488
+pix2 = 648
+Block_size = pix1*pix2*transfer_length  # 316224 Bytes
+
+image = []
+
+buf = bytearray(Block_size);
+dev.ReadFromBlockPipeOut(0xa0, 256, buf);  # Read data from BT PipeOut
 
 #%%
-for i in range (0, 1024, 1):    
-   result = buf[i];
-   print (buf[i])
+counter = 0
+for i in range (0, Block_size, 4):   
+    image.append(buf[i] + (buf[i+1]<<8) + (buf[i+2]<<16)) # transfer length is 16 bits
+    counter = counter + 1
+#    print (buf[i])
 
-    
 #%%
+   
+image = np.array(image)                         # convert list to array
+im_array = np.array(image).reshape(pix1, pix2)  # Reshape array into a 2D array like image
+pic = plt.imshow(im_array, cmap='jet',origin='lower')            # plot image with origin in lower left
+plt.savefig("pic.png")
